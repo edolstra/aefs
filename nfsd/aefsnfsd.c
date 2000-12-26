6a9fc9c3ca43ac2817256035bf643c72f8597b65
Eelco Dolstra <eelco.dolstra@logicblox.com>
2000-12-26 18:45:09 +0000

    Added copyright notices.

--- nfsd/aefsnfsd.c
+++ nfsd/aefsnfsd.c
@@ -1,3 +1,22 @@
+/* aefsnfsd.c -- NFS server front-end to AEFS.
+   Copyright (C) 2000 Eelco Dolstra (edolstra@students.cs.uu.nl).
+
+   $Id: aefsnfsd.c,v 1.4 2000/12/26 19:45:09 eelco Exp $
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
