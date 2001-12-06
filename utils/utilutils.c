a685a03178ef3536b5d0a071385894ea10222718
Eelco Dolstra <eelco.dolstra@logicblox.com>
2001-12-06 08:17:07 +0000

    * Fix indentation, add superblock.h errors.

--- utils/utilutils.c
+++ utils/utilutils.c
@@ -1,7 +1,7 @@
 /* utilutils.c -- Helper routines for the utility programs.
    Copyright (C) 1999, 2001 Eelco Dolstra (eelco@cs.uu.nl).
 
-   $Id: utilutils.c,v 1.9 2001/12/05 09:59:06 eelco Exp $
+   $Id: utilutils.c,v 1.10 2001/12/06 09:17:07 eelco Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -24,6 +24,8 @@
 #include <termios.h>
 
 #include "utilutils.h"
+
+#include "superblock.h"
 
 
 int readKey(char * pszPrompt, unsigned int cbBuffer, char * pszBuffer)
@@ -93,37 +95,41 @@ Cipher * findCipher(Cipher * * papCipher,
 
 char * core2str(CoreResult cr)
 {
-    switch (cr) {
-        case CORERC_OK: return "no error";
-        case CORERC_FILE_NOT_FOUND: return "file not found";
-        case CORERC_NOT_ENOUGH_MEMORY: return "out of memory";
-        case CORERC_FILE_EXISTS: return "file exists";
-        case CORERC_INVALID_PARAMETER: return "invalid parameter";
-        case CORERC_INVALID_NAME: return "invalid file name";
-        case CORERC_BAD_CHECKSUM: return "bad checksum (wrong passphrase?)";
-        case CORERC_BAD_INFOSECTOR: return "bad infosector";
-        case CORERC_NOT_DIRECTORY: return "not a directory";
-        case CORERC_BAD_DIRECTORY: return "corrupt directory";
-        case CORERC_BAD_TYPE: return "bad file type";
-        case CORERC_BAD_EAS: return "corrupt extended attributes";
-        case CORERC_CACHE_OVERFLOW: return "cache overflow";
-        case CORERC_READ_ONLY: return "file system is read-only";
-        case CORERC_ISF_CORRUPT: return "corrupt infosector file";
-        case CORERC_ID_EXISTS: return "file ID exists";
-        case CORERC_SYS + SYS_ACCESS_DENIED:
-            return "access denied to storage file";
-        case CORERC_SYS + SYS_FILE_NOT_FOUND:
-            return "storage file not found";
-        case CORERC_SYS + SYS_FILE_EXISTS:
-            return "storage file exists";
-        case CORERC_SYS + SYS_IO:
-            return "I/O error accessing storage file";
-        case CORERC_SYS + SYS_LOCKED:
-            return "storage file is locked";
-        case CORERC_SYS + SYS_ROFS:
-            return "storage file is on a read-only file system";
-        case CORERC_SYS + SYS_UNKNOWN:
-            return "unknown error accessing storage file";
-        default: return "unknown error";
-    }
+   switch (cr) {
+      case CORERC_OK: return "no error";
+      case CORERC_FILE_NOT_FOUND: return "file not found";
+      case CORERC_NOT_ENOUGH_MEMORY: return "out of memory";
+      case CORERC_FILE_EXISTS: return "file exists";
+      case CORERC_INVALID_PARAMETER: return "invalid parameter";
+      case CORERC_INVALID_NAME: return "invalid file name";
+      case CORERC_BAD_CHECKSUM: return "bad checksum (wrong passphrase?)";
+      case CORERC_BAD_INFOSECTOR: return "bad infosector";
+      case CORERC_NOT_DIRECTORY: return "not a directory";
+      case CORERC_BAD_DIRECTORY: return "corrupt directory";
+      case CORERC_BAD_TYPE: return "bad file type";
+      case CORERC_BAD_EAS: return "corrupt extended attributes";
+      case CORERC_CACHE_OVERFLOW: return "cache overflow";
+      case CORERC_READ_ONLY: return "file system is read-only";
+      case CORERC_ISF_CORRUPT: return "corrupt infosector file";
+      case CORERC_ID_EXISTS: return "file ID exists";
+      case CORERC_BAD_SUPERBLOCK: return "corrupt superblock";
+      case CORERC_UNKNOWN_CIPHER: return "unknown cipher";
+      case CORERC_MISC_CIPHER: return "unknown cryptographic error";
+      case CORERC_BAD_VERSION: return "superblock has bad version";
+      case CORERC_SYS + SYS_ACCESS_DENIED:
+	 return "access denied to storage file";
+      case CORERC_SYS + SYS_FILE_NOT_FOUND:
+	 return "storage file not found";
+      case CORERC_SYS + SYS_FILE_EXISTS:
+	 return "storage file exists";
+      case CORERC_SYS + SYS_IO:
+	 return "I/O error accessing storage file";
+      case CORERC_SYS + SYS_LOCKED:
+	 return "storage file is locked";
+      case CORERC_SYS + SYS_ROFS:
+	 return "storage file is on a read-only file system";
+      case CORERC_SYS + SYS_UNKNOWN:
+	 return "unknown error accessing storage file";
+      default: return "unknown error";
+   }
 }
