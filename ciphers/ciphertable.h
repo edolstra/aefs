27b282f9ac7c55fb69e1af4b42a60b3d8f3780b1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2003-02-16 21:02:04 +0000

    190 copies to tags/branches

---
+++ ciphers/ciphertable.h
@@ -0,0 +1,8 @@
+#ifndef _CIPHERTABLE_H
+#define _CIPHERTABLE_H
+
+#include "cipher.h"
+
+extern Cipher * cipherTable[]; /* null-terminated */
+
+#endif /* !_CIPHERTABLE_H */
---
+++ ciphers/twofish.h
@@ -0,0 +1,8 @@
+#ifndef _TWOFISH_H
+#define _TWOFISH_H
+
+#include "cipher.h"
+
+extern Cipher cipherTwofish;
+
+#endif /* !_TWOFISH_H */
